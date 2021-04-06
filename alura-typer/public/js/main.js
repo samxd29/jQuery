9e4044cf49558388db2d1c30523ea72498ab40ad
Samantha Carvalho Luz <carvalholuzs@gmail.com>
2021-04-06 17:15:41 -0300

    Jquery - jogo de palavras

---
+++ alura-typer/public/js/main.js
@@ -0,0 +1,80 @@
+var campo = $(".campo-digitacao");
+var tempoInicial = $("#tempo-digitacao").text();
+
+$(function(){
+    atualizaTamanhoFrase();
+    inicializaContadores();
+    inicializaCronometro();
+    inicializaMarcadores();
+    $("#botao-reiniciar").click(reiniciaJogo);
+});
+
+function atualizaTamanhoFrase() {
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function inicializaContadores() {
+    campo.on("input", function() {
+        var conteudo = campo.val();
+        
+        var qtdPalavras = conteudo.split(/\S+/).length - 1;
+        $("#contador-palavras").text(qtdPalavras);
+
+        var qtdCaracteres = conteudo.length;
+        $("#contador-caracteres").text(qtdCaracteres);
+    });
+}
+
+function inicializaCronometro() {
+  var tempoRestante = $("#tempo-digitacao").text();
+  campo.one("focus", function () {
+    var cronometroID = setInterval(function () {
+      tempoRestante--;
+      $("#tempo-digitacao").text(tempoRestante);
+      if (tempoRestante < 1) {
+        clearInterval(cronometroID);
+        finalizaJogo();
+      }
+    }, 1000);
+  });
+}
+
+function finalizaJogo() {
+  campo.attr("disabled", true);
+  campo.toggleClass("campo-desativado");
+  inserePlacar();
+}
+
+function inicializaMarcadores() {
+  var frase = $(".frase").text();
+  campo.on("input", function () {
+    var digitado = campo.val();
+    var comparavel = frase.substr(0, digitado.length); // pedaço da frase contando desde a incial até o todo.
+    if (digitado == comparavel) {
+      campo.addClass("borda-correto");
+      campo.removeClass("borda-errado");
+    } else {
+      campo.addClass("borda-errado");
+      campo.removeClass("borda-correto");
+    }
+  });
+}
+
+
+
+
+function reiniciaJogo() {
+    campo.attr("disabled", false);
+    campo.val("");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $("#tempo-digitacao").text(tempoInicial);
+
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-errado");
+    campo.removeClass("borda-correto");
+};
---
+++ alura-typer/public/js/placar.js
@@ -0,0 +1,35 @@
+function inserePlacar(){
+    var corpoTabela = $(".placar").find("tbody"); // .find quer dizer que está dentro do section placar, funciona como uma bolha;
+    var usuario = "Samantha";
+    var numPalavras = $("#contador-palavras").text();
+  
+    var linha = novaLinha(usuario,numPalavras);
+    linha.find(".botao-remover").click(removeLinha);            
+    corpoTabela.prepend(linha);            
+}
+  
+function novaLinha(usuario, palavras) {
+    var linha = $("<tr>"); // elemnto criado com jQuery; 
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+    var link = $("<a>").addClass("botao-remover").attr("href","#"); // attr chamar um atributo e seu valor;
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+  
+    link.append(icone);
+    
+    colunaRemover.append(link);
+  
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+  
+    return linha;
+    
+}
+  
+function removeLinha() {
+      event.preventDefault(); // Quando se tem uma tag a ela vai ou redirecionar a outro link ou redirecionar a um identificador, o event.preventDefault vai remover esse;
+      $(this).parent().parent().remove(); // Como o this é um evento do html e quero trazer para o jQuery é só envolver no $(), comportamento do jQuery;
+      //parent() significa subir um andar ou seja fazer alguma coisa com o pai de uma tag;
+};
